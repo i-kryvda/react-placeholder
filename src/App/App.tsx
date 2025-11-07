789f09a8f5ea7174b609901823ac2d6030891991
schwartz <gtfsolo@gmail.com>
2025-11-08 01:04:31 +0200

    basic localStorage

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,9 +1,42 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 export default function App() {
+  const [value, setValue] = useState<string>(() => {
+    const saved = localStorage.getItem("input");
+
+    return saved ? JSON.parse(saved) : "";
+  });
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setValue(e.target.value);
+
+  const onReset = () => {
+    setValue("");
+    localStorage.removeItem("input");
+  };
+
+  // Зберігаємо при кожній зміні
+  useEffect(() => {
+    localStorage.setItem("input", JSON.stringify(value));
+  }, [value]);
+
   return (
     <>
-      <div>main branch</div>
+      <div>
+        <p>main branch {value}</p>
+        <button type="button" onClick={onReset}>
+          reset
+        </button>
+      </div>
+
+      <input
+        type="text"
+        className="input"
+        placeholder="Enter value"
+        onChange={onChange}
+        value={value}
+      />
     </>
   );
 }
