222149961ecae613ccd3b7a6e832583459a79c8f
schwartz <gtfsolo@gmail.com>
2025-12-03 12:28:10 +0200

    UPDATE getPosts API file

--- src/shared/api/api.tsx
+++ src/shared/api/api.tsx
@@ -1,8 +1,10 @@
 import type { PostItem } from "@app/store/posts/posts.types";
 
 export const api = {
+  url: "https://jsonplaceholder.typicode.com",
+
   getPost: async (): Promise<PostItem[]> => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const response = await fetch(api.url + "/posts");
     if (!response.ok) throw new Error("Network response");
     const data: PostItem[] = await response.json();
     return data;
