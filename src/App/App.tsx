dc2cc2a95081c400020dd456276d0475ae888e5b
schwartz <gtfsolo@gmail.com>
2025-12-07 12:09:09 +0200

    save

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -7,7 +7,14 @@ export default function App() {
         <div className="header__container">header</div>
       </header>
       <main className="main">
-        <div className="main__container">main branch</div>
+        <div className="main__container">
+          <div className="todo">
+            <div className="todo__title">Todo List + Redux</div>
+            <form className="todo__pannel">
+              <input type="text" />
+            </form>
+          </div>
+        </div>
       </main>
       <footer className="footer">
         <div className="footer__container">footer</div>
