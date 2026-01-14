8a7418fae1f5dc971be96bb611bd010e563459c9
schwartz <gtfsolo@gmail.com>
2026-01-14 14:51:14 +0200

    create component sidebar

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,62 +1,14 @@
-import { useState } from "react";
-
-import {
-  MdDashboard,
-  MdPerson,
-  MdSettings,
-  MdLogout,
-  MdMenu,
-} from "react-icons/md";
-
-import { TbAtom2Filled } from "react-icons/tb";
-import { TfiPencilAlt } from "react-icons/tfi";
-// import { FaPenToSquare } from "react-icons/fa6";
-import { IoSearch } from "react-icons/io5";
-import { MdOutlineEventNote } from "react-icons/md";
-import { AiOutlineFileDone } from "react-icons/ai";
+import { Sidebar } from "@components/templates/Sidebar/Sidebar";
 
 import "./App.scss";
 
 export default function App() {
-  const [collapsed, setCollapsed] = useState(false);
-
-  const toggleSidebar = () => setCollapsed((prev) => !prev);
-
   return (
     <>
-      <aside className={`sidebar ${collapsed ? "sidebar--collapsed" : ""}`}>
-        <button className="sidebar__toggle" onClick={toggleSidebar}>
-          <TbAtom2Filled />
-        </button>
-
-        <nav className="sidebar__nav">
-          <ul className="sidebar__list">
-            <li className="sidebar__item">
-              <TfiPencilAlt className="sidebar__icon" />
-              <span className="sidebar__text">Create task</span>
-            </li>
-
-            <li className="sidebar__item">
-              <IoSearch className="sidebar__icon" />
-              <span className="sidebar__text">Search task</span>
-            </li>
-
-            <li className="sidebar__item">
-              <MdOutlineEventNote className="sidebar__icon" />
-              <span className="sidebar__text">Active task</span>
-            </li>
-
-            <li className="sidebar__item sidebar__item--logout">
-              <AiOutlineFileDone className="sidebar__icon" />
-              <span className="sidebar__text">Complete task</span>
-            </li>
-          </ul>
-        </nav>
-      </aside>
-
-      <div className={`app `}>
+      <Sidebar></Sidebar>
+      <div className="app">
         <header className="header">
-          <div className="header__container">Todo List With Redux</div>
+          <div className="header__container">Redux</div>
         </header>
         <main className="main">
           <h1 className="main__container">main</h1>
---
+++ src/components/templates/Sidebar/Sidebar.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { TbAtom2Filled } from "react-icons/tb";
+import { BsLayoutSidebar } from "react-icons/bs";
+import { TfiPencilAlt } from "react-icons/tfi";
+import { IoSearch } from "react-icons/io5";
+import { MdOutlineEventNote } from "react-icons/md";
+import { AiOutlineFileDone } from "react-icons/ai";
+import s from "./Sidebar.module.scss";
+
+export function Sidebar() {
+  const [collapsed, setCollapsed] = useState(false);
+  const toggleSidebar = () => setCollapsed((prev) => !prev);
+  // sidebar ${collapsed ? "sidebar--collapsed" : ""}
+  return (
+    <aside className={s.sidebar + (collapsed ? ` ${s.sidebarCollapsed}` : "")}>
+      <button className={s.sidebarToggle} onClick={toggleSidebar}>
+        <span className={s.sidebarText}>Close</span>
+        <BsLayoutSidebar />
+      </button>
+
+      <nav className={s.sidebarNav}>
+        <ul className={s.sidebarList}>
+          <li className={s.sidebarItem}>
+            <TfiPencilAlt className={s.sidebarIcon} />
+            <span className={s.sidebarText}>Create task</span>
+          </li>
+
+          <li className={s.sidebarItem}>
+            <IoSearch className={s.sidebarIcon} />
+            <span className={s.sidebarText}>Search task</span>
+          </li>
+
+          <li className={s.sidebarItem}>
+            <MdOutlineEventNote className={s.sidebarIcon} />
+            <span className={s.sidebarText}>Active task</span>
+          </li>
+
+          <li className={s.sidebarItem}>
+            <AiOutlineFileDone className={s.sidebarIcon} />
+            <span className={s.sidebarText}>Complete task</span>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+  );
+}
