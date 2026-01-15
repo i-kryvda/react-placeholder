53201f9d54bf394f551be2ab289dab9a4fb0340b
schwartz <gtfsolo@gmail.com>
2026-01-15 21:53:21 +0200

    fix Prevent theme flash (FOUC)

--- src/App/context/ThemeProvider/ThemeProvider.tsx
+++ src/App/context/ThemeProvider/ThemeProvider.tsx
@@ -8,14 +8,19 @@ type ThemeContextType = {
 export const ThemeContext = createContext<ThemeContextType | null>(null);
 
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-  const [theme, setTheme] = useState("dark");
+  const [theme, setTheme] = useState(() => {
+    const saved = localStorage.getItem("theme");
+    return saved ? saved : "dark";
+  });
+
+  useEffect(() => {
+    // localStorage.clear();
+    document.documentElement.dataset.theme = theme;
+    localStorage.setItem("theme", theme);
+  }, [theme]);
 
   const toggleTheme = () =>
     setTheme((prev) => (prev === "dark" ? "light" : "dark"));
-
-  useEffect(() => {
-    document.documentElement.dataset.theme = theme;
-  }, [theme]);
 
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
