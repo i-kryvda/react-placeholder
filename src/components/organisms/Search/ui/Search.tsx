e7b7c03a59496e22d32674c0875af9669d0e68cf
schwartz <gtfsolo@gmail.com>
2026-03-12 18:59:07 +0200

    portal autocomplete search

--- src/components/organisms/Search/ui/Search.tsx
+++ src/components/organisms/Search/ui/Search.tsx
@@ -1,4 +1,5 @@
 import { useAppDispatch, useAppSelector } from "@app/store/store";
+import { createPortal } from "react-dom";
 import { clsx } from "clsx";
 import {
   selectSearchQuery,
@@ -24,27 +25,52 @@ export function Search() {
   const [activeIndex, setActiveIndex] = useState(-1);
   // const isSelectingRef = useRef(false); // ???
   const [isFocused, setIsFocused] = useState(false);
+  // const [isOpen, setIsOpen] = useState(false);
 
   const inputRef = useRef<null | HTMLInputElement>(null);
+  const containerRef = useRef<null | HTMLFormElement>(null);
+  const [position, setPosition] = useState({ top: 0, left: 0, width: 0 });
+
   const { value, onChange, setValue } = useInput(searchQuery);
   const debounced = useDebounce(value, 300);
 
   // правильно це виносити в selector ✔
   const suggestions = useMemo(() => {
-    return (
-      todos
-        .filter((todo) =>
-          todo.title.toLowerCase().startsWith(value.toLowerCase()),
-        )
-        // .sort((a, b) => a.title.localeCompare(b.title))
-        .slice(0, 5)
+    return todos.filter((todo) =>
+      todo.title.toLowerCase().startsWith(value.toLowerCase()),
     );
+    // .sort((a, b) => a.title.localeCompare(b.title))
+    // .slice(0, 3)
   }, [todos, value]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const v = e.target.value;
+    onChange(e);
+
+    if (v.trim()) {
+      setIsFocused(true);
+      const rect = containerRef.current!.getBoundingClientRect();
+
+      setPosition({
+        top: rect.bottom + window.scrollY, // щоб врахувати прокрутку
+        left: rect.left + window.scrollX,
+        width: rect.width,
+      });
+    } else {
+      setIsFocused(false);
+    }
+
+    setActiveIndex(-1);
+  };
 
   const handleSelect = (title: string) => {
     // isSelectingRef.current = true;
     setValue(title);
     dispatch(setSearchQuery(title));
+
+    setIsFocused(false);
+    setActiveIndex(-1);
+    inputRef.current?.blur();
     // setIsFocused(false);
     // setSuggestionsOpen(false);
   };
@@ -104,61 +130,79 @@ export function Search() {
   const isOpen = isFocused && value && suggestions.length > 0;
 
   return (
-    <form className={s.search} onSubmit={(e) => e.preventDefault()}>
+    <form
+      className={s.search}
+      onSubmit={(e) => e.preventDefault()}
+      ref={containerRef}
+    >
       <label htmlFor="search" className="visually-hidden">
         Search Todo
       </label>
 
       <div className={s.searchField}>
         <input
-          value={value}
-          onChange={onChange}
-          onKeyDown={onKeyDown}
-          onFocus={() => setIsFocused(true)}
-          onBlur={() => setIsFocused(false)}
-          type="search"
           ref={inputRef}
+          value={value}
+          onChange={handleChange}
+          onKeyDown={onKeyDown}
+          // onFocus={() => setIsFocused(true)}
+          // onBlur={() => setIsFocused(false)}
+          type="search"
           id="search"
           placeholder="Search Todo..."
           role="combobox"
-          // onBlur={() => setSuggestionsOpen(false)}
-
           aria-autocomplete="list"
           aria-controls="suggestions-list"
           autoComplete="off"
         />
       </div>
 
-      {isOpen && (
-        <ul className={s.suggestions} id="suggestions-list" role="listbox">
-          {suggestions.map((item, index) => (
-            <li
-              id="option-0"
-              role="option"
-              key={item.id}
-              className={
-                index === activeIndex
-                  ? `${s.suggestion} ${s.active}`
-                  : `${s.suggestion}`
-              }
-              aria-selected={index === activeIndex}
-              onMouseEnter={() => setActiveIndex(index)}
-              onMouseDown={() => handleSelect(item.title)}
-            >
-              {/* {item.title} */}
-              {getHighlightParts(item.title, searchQuery).map((part, i) =>
-                part.isMatch ? (
-                  <mark key={i} className={s.highlight}>
-                    {part.text}
-                  </mark>
-                ) : (
-                  <span key={i}>{part.text}</span>
-                ),
-              )}
-            </li>
-          ))}
-        </ul>
-      )}
+      {isOpen &&
+        createPortal(
+          <ul
+            className={s.suggestions}
+            id="suggestions-list"
+            role="listbox"
+            style={{
+              position: "absolute",
+              top: position.top,
+              left: position.left,
+              width: position.width,
+              // width: "100",
+              border: "1px solid #ccc",
+              zIndex: 9999,
+            }}
+          >
+            {suggestions.map((item, index) => (
+              <li
+                id="option-0"
+                role="option"
+                key={item.id}
+                className={
+                  index === activeIndex
+                    ? `${s.suggestion} ${s.active}`
+                    : `${s.suggestion}`
+                }
+                aria-selected={index === activeIndex}
+                onMouseEnter={() => setActiveIndex(index)}
+                onMouseDown={() => handleSelect(item.title)}
+              >
+                {/* {item.title} */}
+                {getHighlightParts(item.title, searchQuery).map((part, i) =>
+                  part.isMatch ? (
+                    <mark key={i} className={s.highlight}>
+                      {part.text}
+                    </mark>
+                  ) : (
+                    <span key={i}>{part.text}</span>
+                  ),
+                )}
+              </li>
+            ))}
+          </ul>,
+
+          document.body,
+        )}
     </form>
   );
 }
